818160bd23fb2dcd9e15825897cbd0ea08757ef5
marco-esposito <m.espo@tiscali.it>
2018-05-31 16:13:58 +0200

    01 basic routing

--- 01-basic-routing/src/components/App.js
+++ 01-basic-routing/src/components/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import Hello from './HelloComponent';
+import About from './AboutComponent';
+import Books from './BooksComponent';
+import Home from './HomeComponent';
+
 // import route Components here
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
-
+// Put in this component what you want to be shown on each page (e.g. nav bar)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <ul>
-            <li><a href="">Hello</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Books</a></li>
-          </ul>
-          <hr/>
-          {/* Routes will go here */}
-      </div>
-  </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <ul>
+              <li><Link to="/hello">Hello</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/books">Books</Link></li>
+            </ul>
+            <hr/>
+            {/* Router inject the HISTORY API OBJECT in its children as props */}
+            {/* History API object contains: history, location, match */}
+            <Route exact path="/" component={Home} />
+            <Route path="/hello" component={Hello} />
+            <Route path="/about" component={About} />
+            <Route path="/books" component={Books} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
 export default App;
+
+// NOTE:
+// The History API is an object that lets us manage the current location via history.location as well as previous locations. Think of the location property of the object as an array. The current location is the last item on the array and we manipulate the array through methods such as history.push() orhistory.replace. Whatever manipulation is made on the array will trigger a page transition to the current location. This is what happens behind the scene when using Link and Redirect component as we’ll see soon.
--- 02-nested-routing/src/components/BooksComponent.js
+++ 02-nested-routing/src/components/BooksComponent.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
-const Books = () => {
+const Books = ({ match }) => {
     return (
-        <div className="jumbotron">
-            <h1 className="display-3">My Books</h1>
-        </div>
+<div>
+   <div className="jumbotron">
+        <h1 className="display-3">My Books</h1>
+   </div>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3">
+          <ul>
+            <li><Link to="">HTML</Link></li>
+            <li><Link to="">CSS</Link></li>
+            <li><Link to="">React</Link></li>
+          </ul>
+      </div>
+      <div className="col-md-9">
+               {/* place routes here */}
+      </div>
+    </div>
+   </div>
+</div>
     );
 }
 
